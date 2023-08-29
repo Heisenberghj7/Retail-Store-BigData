67f0049e0629f4e7b3ed3ff187739a1ec92f6357
Heisenberg <medhajjari7@gmail.com>
2023-08-29 14:38:00 +0100

    working pyspark script

--- retail_pyspark.py
+++ retail_pyspark.py
@@ -1,19 +1,20 @@
 from pyspark.sql import SparkSession
 
 # Create a Spark session
+
 spark = SparkSession.builder.appName("RetailStoreData").getOrCreate()
 
 # Define the HDFS base path
-hdfs_path_starting = "/user/"  
-hdfs_path_finishing = "/part-m-00000.csv" 
+hdfs_path_starting = "hdfs://sandbox-hdp.hortonworks.com:8020//user/"  
+hdfs_path_finishing = "/part-m-00000" 
 
 # Read data from HDFS into DataFrames
-customers_df = spark.read.format("csv").option("header", "true").load(hdfs_base_path + "customers" + hdfs_path_finishing )
-departments_df = spark.read.format("csv").option("header", "true").load(hdfs_base_path + "departments" + hdfs_path_finishing)
-categories_df = spark.read.format("csv").option("header", "true").load(hdfs_base_path + "categories" + hdfs_path_finishing)
-orders_df = spark.read.format("csv").option("header", "true").load(hdfs_base_path + "orders" + hdfs_path_finishing)
-order_items_df = spark.read.format("csv").option("header", "true").load(hdfs_base_path + "order_items" + hdfs_path_finishing)
-products_df = spark.read.format("csv").option("header", "true").load(hdfs_base_path + "products" + hdfs_path_finishing)
+customers_df = spark.read.format("csv").option("header", "false").load(hdfs_path_starting + "customers" + hdfs_path_finishing )
+departments_df = spark.read.format("csv").option("header", "false").load(hdfs_path_starting + "departments" + hdfs_path_finishing)
+categories_df = spark.read.format("csv").option("header", "false").load(hdfs_path_starting + "categories" + hdfs_path_finishing)
+orders_df = spark.read.format("csv").option("header", "false").load(hdfs_path_starting + "orders" + hdfs_path_finishing)
+order_items_df = spark.read.format("csv").option("header", "false").load(hdfs_path_starting + "order_items" + hdfs_path_finishing)
+products_df = spark.read.format("csv").option("header", "false").load(hdfs_path_starting + "products" + hdfs_path_finishing)
 
 # Now you have DataFrames for each of your data tables
 
